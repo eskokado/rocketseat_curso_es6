44e84027d30697f2423120d19addece3e7c55e75
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-15 14:02:45 -0300

    Fundamentos ES6

--- bundle.js
+++ bundle.js
@@ -1,27 +1,119 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-alert('Teste');
-
-var Teste =
-/*#__PURE__*/
-function () {
-  function Teste() {
-    _classCallCheck(this, Teste);
+var usuario = {
+  nome: 'Edson',
+  idade: 53,
+  endereco: {
+    cidade: 'São Paulo',
+    estado: 'SC'
   }
+};
+console.log(usuario);
 
-  _createClass(Teste, [{
-    key: "metodo",
-    value: function metodo() {}
-  }, {
-    key: "outro",
-    value: function outro() {}
-  }]);
+function mostrarNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade,
+      cidade = _ref.endereco.cidade;
+  console.log(nome, idade, cidade);
+}
 
-  return Teste;
-}();
+mostrarNome(usuario); // const nome = usuario.nome;
+// const idade = usuario.idade;
+//const cidade = usuario.endereco.cidade;
+// const { nome, idade, endereco: { cidade } } = usuario;
+// console.log(nome, idade, cidade);
+// exemplos de valores padrao
+// const soma1 = (a = 7, b = 9) => a + b;
+// console.log(soma1(51));
+// console.log(soma1());
+// function soma(a = 7, b = 7) {
+//     return a + b;
+// }
+// console.log(soma(1));
+// console.log(soma());
+// exemplos de uso de arrows
+// const arr = [1,2,3,4,5,7,8,9];
+// const newArr =  arr.map(item => item * 2);
+// console.log(newArr);
+// const teste = () => {
+//     return "Teste";
+// }
+// console.log(teste());
+// const teste1 = () => "Teste1";
+// console.log(teste1());
+// // para objeto precisa usar ()
+// const teste3 = () => ({usuario: 'eskokado'});
+// console.log(teste3());
+// Exemplos de uso de array
+// const arr = [1,2,3,4,5,7,8,9];
+// const newArr =  arr.map(function(item, index) {
+//     return item + index;
+// });
+// console.log(newArr);
+// const sum = arr.reduce(function(total, next){
+//     console.log(total, next);
+//     return total + next;
+// });
+// console.log(sum);
+// const filter = arr.filter(function(item){
+//     return item % 2 === 0;
+// });
+// console.log(filter);
+// const find = arr.find(function(item){
+//     return item === 4;
+// });
+// console.log(find);
+// Exempo de uma classe com methodo statico
+// class Matematica {
+//     static soma (a, b) {
+//         return a + b;        
+//     }
+// }
+// console.log(Matematica.soma(5,4));
+// Exemplo 3 - Metodo estatico não reconhe outros restantes da classe.
+// class TodoList {
+//     constructor() {
+//         this.todos = [];
+//     }
+//     static addTodo() {
+//         this.todos.push('Novo todo');
+//         console.log(this.todos);
+//     }
+// }
+// TodoList.addTodo();
+// TodoList.addTodo();
+// TodoList.addTodo();
+// TodoList.addTodo();
+// Exemplo 2 - Herança
+// class List {
+//     constructor() {
+//         this.data = [];
+//     }
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
+// }
+// class TodoList extends List{
+//     constructor() {
+//         super();
+//         this.usuario = "Edson Shideki Kokado";
+//     }
+//     mostraUsuario() {
+//         console.log(this.usuario);
+//     }
+//     // ######### Exemplo 1 - sem herança
+//     // constructor() {
+//     //     this.todos = [];
+//     // }
+//     // addTodo() {
+//     //     this.todos.push('Novo todo');
+//     //     console.log(this.todos);
+//     // }
+// }
+// var MinhaLista = new TodoList();
+// document.getElementById('novotodo').onclick = function() {
+//     // MinhaLista.addTodo();
+//     MinhaLista.add('Novo todo por herança');
+// }
+// MinhaLista.mostraUsuario();
--- main.js
+++ main.js
@@ -1,11 +1,156 @@
-alert('Teste');
+const usuario = {
+    nome: 'Edson',
+    idade: 53,
+    endereco: {
+        cidade: 'São Paulo',
+        estado: 'SC',
+    },
+};
 
-class Teste {
-    metodo() {
+console.log(usuario);
 
-    }
-
-    outro() {
-        
-    }
+function mostrarNome({ nome, idade, endereco: { cidade } }) {
+    console.log(nome, idade, cidade);
 }
+
+mostrarNome(usuario);
+
+// const nome = usuario.nome;
+// const idade = usuario.idade;
+//const cidade = usuario.endereco.cidade;
+
+// const { nome, idade, endereco: { cidade } } = usuario;
+
+// console.log(nome, idade, cidade);
+
+// exemplos de valores padrao
+// const soma1 = (a = 7, b = 9) => a + b;
+// console.log(soma1(51));
+// console.log(soma1());
+
+
+// function soma(a = 7, b = 7) {
+//     return a + b;
+// }
+
+// console.log(soma(1));
+// console.log(soma());
+
+// exemplos de uso de arrows
+// const arr = [1,2,3,4,5,7,8,9];
+
+// const newArr =  arr.map(item => item * 2);
+
+// console.log(newArr);
+
+// const teste = () => {
+//     return "Teste";
+// }
+// console.log(teste());
+
+// const teste1 = () => "Teste1";
+
+// console.log(teste1());
+
+// // para objeto precisa usar ()
+// const teste3 = () => ({usuario: 'eskokado'});
+// console.log(teste3());
+
+
+// Exemplos de uso de array
+// const arr = [1,2,3,4,5,7,8,9];
+
+// const newArr =  arr.map(function(item, index) {
+//     return item + index;
+// });
+
+// console.log(newArr);
+
+// const sum = arr.reduce(function(total, next){
+//     console.log(total, next);
+//     return total + next;
+// });
+
+// console.log(sum);
+
+// const filter = arr.filter(function(item){
+//     return item % 2 === 0;
+// });
+
+// console.log(filter);
+
+// const find = arr.find(function(item){
+//     return item === 4;
+// });
+
+// console.log(find);
+
+// Exempo de uma classe com methodo statico
+// class Matematica {
+//     static soma (a, b) {
+//         return a + b;        
+//     }
+// }
+
+// console.log(Matematica.soma(5,4));
+
+// Exemplo 3 - Metodo estatico não reconhe outros restantes da classe.
+// class TodoList {
+//     constructor() {
+//         this.todos = [];
+//     }
+
+//     static addTodo() {
+//         this.todos.push('Novo todo');
+//         console.log(this.todos);
+//     }
+// }
+
+// TodoList.addTodo();
+// TodoList.addTodo();
+// TodoList.addTodo();
+// TodoList.addTodo();
+
+// Exemplo 2 - Herança
+// class List {
+//     constructor() {
+//         this.data = [];
+//     }
+
+//     add(data) {
+//         this.data.push(data);
+//         console.log(this.data);
+//     }
+// }
+
+// class TodoList extends List{
+//     constructor() {
+//         super();
+
+//         this.usuario = "Edson Shideki Kokado";
+//     }
+
+//     mostraUsuario() {
+//         console.log(this.usuario);
+//     }
+
+
+//     // ######### Exemplo 1 - sem herança
+//     // constructor() {
+//     //     this.todos = [];
+//     // }
+
+//     // addTodo() {
+//     //     this.todos.push('Novo todo');
+//     //     console.log(this.todos);
+//     // }
+// }
+
+// var MinhaLista = new TodoList();
+
+// document.getElementById('novotodo').onclick = function() {
+//     // MinhaLista.addTodo();
+//     MinhaLista.add('Novo todo por herança');
+// }
+
+// MinhaLista.mostraUsuario();
